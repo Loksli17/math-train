ebd4f8e7fe9c10404646ff77036f7d11f862939b
BlackLirium <mrfilin97@gmail.com>
2019-08-09 16:05:17 +1000

    createCatalog

--- commands/createCatalogs.js
+++ commands/createCatalogs.js
@@ -3,39 +3,76 @@ const mysql = require('../lib/database/mysql');
 const async      = require('async');
 let query = async () => {
     let remove = await mysql.promise().query('Delete from catalog where 1');
-    let mysqlQuary = "Insert into catalog(title,catalog_group_id) VALUES(?,?)"
-    let create = await await  async.parallel([
-                                function (callback) {
-                                    let catalog = ['Math',1];
-                                    mysql.query(mysqlQuary,catalog,function (err,result) {
-                                        if (err){
-                                            console.log(err);
-                                            throw err;
-                                        }else{
-                                            callback(err,result);
-                                        }
-                                    })
-                                },
-                                function () {
-                                    let catalog = ['',2];
-                                    mysql.query(mysqlQuary,catalog,function (err,result) {
-                                        if (err){
-                                            console.log(err);
-                                            throw err;
-                                        }else{
-                                            callback(err,result);
-                                        }
-                                    })
-                                },function(){
-                                    let catalog = []
+    let mysqlQuary = "Insert into catalog(title,catalog_group_id) VALUES(?,?)";
+    let catalogs = [
+        {
+            id              : 1,
+            title           : 'Легко',
+            catalog_group_id: 1 ,
+        },
+        {
+            id              : 2,
+            title           : 'Средне',
+            catalog_group_id: 2 ,
+        },
+        {
+            id              : 3,
+            title           : 'Сложно',
+            catalog_group_id: 3 ,
+        },
+        {
+            id              : 4,
+            title           : 'Очень сложно',
+            catalog_group_id: 3,
+        },
 
-
-
-                                },
-
-
-
-                              ])
+    ];
+    let create = await async.parallel([
+            function(callback){
+                mysql.promise().query(mysqlQuary, catalogs[0], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+            function(callback){
+                mysql.promise().query(mysqlQuary, catalogs[1], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+            function(callback){
+                mysql.promise().query(mysqlQuary, catalogs[2], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+            function(callback){
+                mysql.promise().query(mysqlQuary, catalogs[4], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+        ],
+        function(err, result){
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            console.log(result);
+        }
+    )
 };
 
 query();
