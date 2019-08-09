7754fc6970179520c5f6097ecb4e1cb8abef7800
BlackLirium <mrfilin97@gmail.com>
2019-08-09 16:14:11 +1000

    createCtalog

--- commands/createCatalogs.js
+++ commands/createCatalogs.js
@@ -1,32 +1,35 @@
 const config   = require('../config');
 const mysql = require('../lib/database/mysql');
 const async      = require('async');
-let query = async () => {
-    let remove = await mysql.promise().query('Delete from catalog where 1');
-    let mysqlQuary = "Insert into catalog(title,catalog_group_id) VALUES(?,?)";
-    let catalogs = [
-        {
-            id              : 1,
-            title           : 'Легко',
-            catalog_group_id: 1 ,
-        },
-        {
-            id              : 2,
-            title           : 'Средне',
-            catalog_group_id: 2 ,
-        },
-        {
-            id              : 3,
-            title           : 'Сложно',
-            catalog_group_id: 3 ,
-        },
-        {
-            id              : 4,
-            title           : 'Очень сложно',
-            catalog_group_id: 3,
-        },
 
-    ];
+let mysqlQuary = "Insert into `catalog` SET ?";
+let catalogs = [
+    {
+        id              : 1,
+        title           : 'Легко',
+        catalog_group_id: 1 ,
+    },
+    {
+        id              : 2,
+        title           : 'Средне',
+        catalog_group_id: 2 ,
+    },
+    {
+        id              : 3,
+        title           : 'Сложно',
+        catalog_group_id: 3 ,
+    },
+    {
+        id              : 4,
+        title           : 'Очень сложно',
+        catalog_group_id: 3,
+    },
+
+];
+
+let query = async () => {
+    let remove = await mysql.promise().query('Delete from `catalog` WHERE 1');
+
     let create = await async.parallel([
             function(callback){
                 mysql.promise().query(mysqlQuary, catalogs[0], function(err, result){
