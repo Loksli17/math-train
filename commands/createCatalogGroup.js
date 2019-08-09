fa565fc89fcb50157e3cac1aef40ccc05384eeef
Andrey M <ami0504@mail.ru>
2019-08-09 15:45:01 +0930

    рефактор создания пользователя + создание группы каталога

---
+++ commands/createCatalogGroup.js
@@ -0,0 +1,63 @@
+const mysql = require('../lib/database/mysql');
+const async = require('async');
+
+let groups = [
+    {
+        id: '1',
+        title: 'Сложности',
+    },
+    {
+        id: '2',
+        title: 'Название',
+    },
+    {
+        id: '3',
+        title: 'Название',
+    }
+];
+
+let queryStr = 'INSERT INTO `catalog_group` SET ?';
+
+let query = async () => {
+    let remove = await mysql.promise().query('DELETE FROM `catalog_group` WHERE 1');
+
+    let create = await async.parallel([
+            function(callback){
+                mysql.promise().query(queryStr, groups[0], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+            function(callback){
+                mysql.promise().query(queryStr, groups[1], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+            function(callback){
+                mysql.promise().query(queryStr, groups[2], function(err, result){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, result);
+                });
+            },
+        ],
+        function(err, result){
+            if(err){
+                console.log(err);
+                throw err;
+            }
+            console.log(result);
+        }
+    )
+}
+
+query();
--- commands/createUsers.js
+++ commands/createUsers.js
@@ -9,117 +9,112 @@ let query = async () => {
     let remove = await User.remove({});
 
     let create = await async.parallel([
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Олежа',
-                                    secondName: 'Чеботарев',
-                                },
-                                email: 'ami0504@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
-                            });
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
+            function(callback){
+                let user = new User({
+                    name: {
+                        firstName: 'Олежа',
+                        secondName: 'Чеботарев',
+                    },
+                    email: 'ami0504@mail.ru',
+                    pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
+                });
+                user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+            function(callback){
+                let user = new User({
+                        name: {
+                            firstName: 'Вася',
+                            secondName: 'Чеботарев',
                         },
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Вася',
-                                    secondName: 'Чеботарев',
-                                },
-                                email: 'ami0505@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('1234').digest('hex'),
-                            });
-
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
-                        },
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Юха',
-                                    secondName: 'Спицин',
-                                },
-                                email: 'ami0506@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
-                            });
-
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
-                        },
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Мазда',
-                                    secondName: 'Хонда',
-                                },
-                                email: 'ami0507@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
-                            });
-
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
-                        },
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Дмитрий',
-                                    secondName: 'Белан',
-                                },
-                                email: 'ami0508@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
-                            });
-
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
-                        },
-                        function(callback){
-                            let user = new User({
-                                name: {
-                                    firstName: 'Олежа',
-                                    secondName: 'Рукавишников',
-                                },
-                                email: 'ami0509@mail.ru',
-                                pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
-                            });
-
-                            user.save(function(err){
-                                if(err){
-                                    console.log(err);
-                                    throw err;
-                                }
-                                callback(err, user);
-                            });
-                        },
-                    ],
-                    function(err, result){
-                        console.log(result);
+                        email: 'ami0505@mail.ru',
+                        pass: crypto.createHash('sha256', config.user.passSecret).update('1234').digest('hex'),
+                });
+                user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+            function(callback){
+                let user = new User({
+                    name: {
+                        firstName: 'Юха',
+                        secondName: 'Спицин',
+                    },
+                    email: 'ami0506@mail.ru',
+                    pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
+                });
+                user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+            function(callback){
+                let user = new User({
+                    name: {
+                        firstName: 'Мазда',
+                        secondName: 'Хонда',
+                    },
+                    email: 'ami0507@mail.ru',
+                    pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
                     });
+                 user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+            function(callback){
+                let user = new User({
+                    name: {
+                        firstName: 'Дмитрий',
+                        secondName: 'Белан',
+                    },
+                    email: 'ami0508@mail.ru',
+                    pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
+                });
+                user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+            function(callback){
+                let user = new User({
+                    name: {
+                        firstName: 'Олежа',
+                        secondName: 'Рукавишников',
+                    },
+                    email: 'ami0509@mail.ru',
+                    pass: crypto.createHash('sha256', config.user.passSecret).update('123').digest('hex'),
+                });
+                user.save(function(err){
+                    if(err){
+                        console.log(err);
+                        throw err;
+                    }
+                    callback(err, user);
+                });
+            },
+        ],
+        function(err, result){
+            console.log(result);
+        });
 }
 
 query();
