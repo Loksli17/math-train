b47f2a098b28717ca3f05fb8f98bbb7bdbc5e331
Andrey M <ami0504@mail.ru>
2019-08-23 21:47:27 +0930

    ORM mysql ver=0.5

--- models/mysql/taskModel.js
+++ models/mysql/taskModel.js
@@ -1,1 +1,17 @@
+const modelMysql = require('../../lib/Orm/mysqlOrm');
+
 //model of tasks-mysql
+function TaskModel(){
+    tableName = 'Task';
+    modelMysql.call(this, tableName);
+}
+
+//наследование
+TaskModel.prototype = Object.create(modelMysql.prototype);
+TaskModel.prototype.constructor = TaskModel;
+
+TaskModel.prototype.rules = {
+    required: ['title', 'text', 'description', 'catalog_id'],
+}
+
+module.exports = TaskModel;
