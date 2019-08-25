8275a2864ba1f60fa40f061c84f925d6b5cfa033
Andrey M <ami0504@mail.ru>
2019-08-25 20:13:45 +0930

    ORM mysql ver=1.0

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,12 @@ const TaskModel = require('../models/mysql/TaskModel');
 
 exports.actionIndex = async function(req, res){
     let Task = new TaskModel();
-    let tasks = await Task.save({title: 'task', text: 'text', description: 'desc', catalog_id: 1, isReady: 0});
+    let tasks = await Task.save({
+        title: 'название',
+        text: 'текст',
+        description: 'описание',
+        catalog_id: 1,
+    });
     console.log(tasks);
-    res.send(tasks);
+    res.send();
 }
