8836627865e2d4574411b2f86c535e6695dbcc8d
BlackLirium <mrfilin97@gmail.com>
2019-08-26 17:10:36 +1000

    taskFilter

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,21 +2,16 @@ const TaskModel = require('../models/mysql/TaskModel');
 
 
 exports.actionIndex = async function(req, res){
-<<<<<<< HEAD
+
     // let Task = new TaskModel();
-    // let tasks = await Task.save({title: 'task', text: 'text', description: 'desc', catalog_id: 1, isReady: 0});
+    // let tasks = await Task.save({
+    //     title: 'название',
+    //     text: 'текст',
+    //     description: 'описание',
+    //     catalog_id: 1,
+    // });
     // console.log(tasks);
+    // res.send();
     res.render('index');
 };
-=======
-    let Task = new TaskModel();
-    let tasks = await Task.save({
-        title: 'название',
-        text: 'текст',
-        description: 'описание',
-        catalog_id: 1,
-    });
-    console.log(tasks);
-    res.send();
-}
->>>>>>> 2abc892f1a8d2f7982282c876ad5541093a7ad7c
+
