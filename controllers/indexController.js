858734751570150582ded8d141424e0632bd3750
Andrey M <ami0504@mail.ru>
2019-09-18 23:12:10 +0930

    ORM рефакторинг

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -6,13 +6,14 @@ exports.actionIndex = async function(req, res){
     let Task = new TaskModel();
     let Post = new PostModel();
 
-    // let tasks = await Task.find('all', {
-    //     like: [
-    //         ['text', '%о%'],
-    //     ],
-    // });
-    //
-    // console.log(tasks);
+    let tasks = await Task.find('all', {
+        like: [
+            ['text', '%о%'],
+        ],
+        where: 'id = 1',
+    });
+
+    console.log(tasks);
 
     let save = await Post.save({title: 'название', text: 'текст', image: '1.jpg', view: 1, description: null});
     // console.log(save);
