5b8b92fbb6847e950714ee125e176b6222be6a54
San-sanichick <53393830+San-sanichick@users.noreply.github.com>
2019-09-27 17:20:14 +1000

    прокачка модели отношений

--- resources/trains/models/RelationModel.js
+++ resources/trains/models/RelationModel.js
@@ -1,7 +1,16 @@
-function Relation(set, size) {
-    this.set = set;
+function Relation(set, size, graph, mat) {
     this.relation = new Array();
-    this.size = size;
+    this.set;
+    this.size;
+    this.graph;
+    this.mat;
+    if (graph != undefined && mat != undefined && !set && !size) {
+        this.graph = graph;
+        this.mat = mat;
+    } else {
+        this.set = set;
+        this.size = size;
+    }
 }
 
 Relation.prototype.randomInteger = function(min, max) {
@@ -9,16 +18,27 @@ Relation.prototype.randomInteger = function(min, max) {
     rand = Math.round(rand);
     return rand;
 };
+
 //это дело наверняка можно сделать красивее
 Relation.prototype.getRel = function() {
-    let oper = new Operation(0, 0, 0, 0, 1);
-    let alg = new Algebra();
+    if (this.graph == undefined) {
+        let oper = new Operation(0, 0, 0, 0, 1);
+        let alg = new Algebra();
 
-    let res = alg.do(this.set, this.set, oper).elems.slice();
-    for (let i = 0; i < this.size; i++) {
-        let index = this.randomInteger(0, res.length - 1);
-        this.relation.push(res[index]);
-        res.splice(index, 1);
+        let res = alg.do(this.set, this.set, oper).elems.slice();
+        for (let i = 0; i < this.size; i++) {
+            let index = this.randomInteger(0, res.length - 1);
+            this.relation.push(res[index]);
+            res.splice(index, 1);
+        }
+    } else {
+        for (let i = 0; i < graph.countVertex; i++) {
+            for (let j = 0; j < graph.countVertex; j++) {
+                if (this.mat[i][j]) {
+                    this.relation.push(new Set(2, 0, [String.fromCharCode(graph.vertex[i].name), String.fromCharCode(graph.vertex[j].name)]));
+                }
+            }
+        }
     }
     return this.relation;
 }
